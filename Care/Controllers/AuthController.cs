95c8ab49b3c6b290e6fd28883a3039d3d64a1f1f
Nenartavičiūtė Laura <laura.nenartaviciute@mif.stud.vu.lt>
2021-09-20 11:51:19 +0300

    Add modal form functionality

---
+++ Care/Controllers/AuthController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Care.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return PartialView("/Views/Shared/_Login.cshtml");
+        }
+
+    }
+}
--- Care/wwwroot/js/site.js
+++ Care/wwwroot/js/site.js
@@ -1,4 +1,18 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿//  https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 
-// Write your JavaScript code.
+$("#loginBtn").click(function () {
+
+    var url = "/Auth/Login";
+    $.ajax({
+        url: url,
+        cache: false,
+        type: "GET",
+        success: function (data) {
+            $(data).modal('show');
+        },
+        error: function (reponse) {
+            alert("error : " + reponse);
+        }
+    });
+});
+
