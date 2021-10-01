66a16211691ad0f7cf522490ccfd18a7fb0a1f0b
Nenartavičiūtė Laura <laura.nenartaviciute@mif.stud.vu.lt>
2021-10-01 13:41:14 +0300

    Add es6 and three.js

---
+++ Care/webpack.config.js
@@ -0,0 +1,22 @@
+﻿const path = require('path');
+module.exports = {
+    entry: ['./Scripts/es6/main.js'],
+    output: {
+        path: path.resolve(__dirname, './Scripts/build'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node_modules/
+        },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }]
+    }
+}
