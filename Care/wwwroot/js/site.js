be04d507fb5c6fa30476e5c5b076538acd44ed32
geigosam <56157793+geigosam@users.noreply.github.com>
2021-10-07 16:36:36 +0300

    Updated auth page design

--- Care/wwwroot/js/site.js
+++ Care/wwwroot/js/site.js
@@ -1,35 +1,6 @@
 ﻿//  https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 
-$("#loginBtn").click(function () {
-
-    var url = "/Auth/LogIn";
-    $.ajax({
-        url: url,
-        cache: false,
-        type: "GET",
-        success: function (data) {
-            $(data).modal('show');
-        },
-        error: function (reponse) {
-            alert("error : " + reponse);
-        }
-    });
-});
-
-function chooseLogin () {
-    var loginUser = document.getElementById("LoginUser");
-    var loginAdmin = document.getElementById("LoginAdmin");
-    var url;
-
-    if (loginUser.checked) {
-        url = "/Auth/UserLogIn";
-    }
-    else if (loginAdmin.checked) {
-        url = "/Auth/AdminLogIn";
-    }
-    else {
-        url = "/"
-    }
+function openSite(url) {
     $.ajax({
         url: url,
         cache: false,
@@ -42,19 +13,3 @@ function chooseLogin () {
         }
     });
 };
-
-$("#registrationBtn").click(function () {
-
-    var url = "/Auth/SignUp";
-    $.ajax({
-        url: url,
-        cache: false,
-        type: "GET",
-        success: function (data) {
-            $(data).modal('show');
-        },
-        error: function (reponse) {
-            alert("error : " + reponse);
-        }
-    });
-});
