f23392270ad92c92a9b4db184f5512d423606a69
Domantas Gruodis <domantas.gruod@gmail.com>
2021-10-08 14:15:08 +0300

    Creating required files for the post creation

---
+++ Care/Models/PostModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+namespace Care.Models
+{
+    public class PostModel
+    {
+        [Required]
+        [DisplayName("Organization Name")]
+        public string OrgName { get; set; }
+        [DisplayName("Short Organization Description")]
+        public string OrgShortDescr { get; set; }
+        [Required]
+        [DisplayName("Long Organization Description")]
+        public string OrgLongDescr { get; set; }
+        [Required]
+        [DisplayName("Organization Logo")]
+        public string OrgLogo { get; set; }
+        [DisplayName("Organization Photo")]
+        public string OrgPhoto { get; set; }
+    }
+}
