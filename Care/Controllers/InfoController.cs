9af598d790455b0307240bde27a26cdf9295623d
geigosam <56157793+geigosam@users.noreply.github.com>
2021-10-14 16:45:53 +0300

    Connected info pages to the database

--- Care/Controllers/InfoController.cs
+++ Care/Controllers/InfoController.cs
@@ -3,22 +3,24 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Care.Models;
 
 namespace Care.Controllers
 {
     public class InfoController : Controller
     {
+        private readonly ServiceDbContext _context;
+
+        public InfoController(ServiceDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index(string id)
         {
             try {
                 UInt32 idInt = UInt32.Parse(id);
-                Models.InfoViewModel orgInfo = new Models.InfoViewModel {
-                    Title = "Organisation no. " + idInt,
-                    LongDescription = "This is the long description of organisation no. " + idInt + ". So far the database isn't ready, so the only thing that changes in the page is the ID. However, the homepage and donate links currently work as previous/next buttons to test if I can change hyperlinks dynamically.",
-                    HomeLink = "https://localhost:5001/Info/Index/" + (idInt - 1),
-                    DonateLink = "https://localhost:5001/Info/Index/" + (idInt + 1)
-                };
-                return View(orgInfo);
+                var org = _context.Posts.FirstOrDefault(m => m.OrgId == idInt);
+                return View(org);
             }
             catch {
                 ModelState.AddModelError("ID parsing", "Organisation ID must be an integer!");
---
+++ Care/Models/OrgModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Care.Models
+{
+    public class OrgModel
+    {
+        [Required]
+        [Key]
+        public int OrgId { get; set; }
+        [Required]
+        public string OrgName { get; set; }
+        public string OrgShortDescr { get; set; }
+        [Required]
+        public string OrgLongDescr { get; set; }
+        public string OrgLink { get; set; }
+        [Required]
+        public string OrgLogo { get; set; }
+        public string OrgPhoto { get; set; }
+
+    }
+}
