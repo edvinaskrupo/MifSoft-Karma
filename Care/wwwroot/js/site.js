5c7001c49bd784a26dee02a6e53fe52d01313af5
Nenartavičiūtė Laura <laura.nenartaviciute@mif.stud.vu.lt>
2021-10-14 20:23:46 +0300

    Final carousel changes

--- Care/wwwroot/js/site.js
+++ Care/wwwroot/js/site.js
@@ -14,4 +14,12 @@ function openSite(url) {
     });
 };
 
+var myelement = document.getElementById("bootsrap-Carousel");
 
+function changeByScroll() {
+    $('.carousel').carousel('next')
+}
+
+myelement.addEventListener('wheel', function (e) {
+    changeByScroll();
+});
