d997d8feda36187daef269aa8ae8a1830f87c726
Domantas Gruodis <domantas.gruod@gmail.com>
2021-10-14 23:43:57 +0300

    Solving Merge Conflict

--- Care/wwwroot/js/site.js
+++ Care/wwwroot/js/site.js
@@ -31,19 +31,3 @@ $("#registrationBtn").click(function () {
         }
     });
 });
-
-$("#postBtn").click(function () {
-
-    var url = "/Post/AddOrg";
-    $.ajax({
-        url: url,
-        cache: false,
-        type: "GET",
-        success: function (data) {
-            $(data).modal('show');
-        },
-        error: function (reponse) {
-            alert("error : " + reponse);
-        }
-    });
-});
