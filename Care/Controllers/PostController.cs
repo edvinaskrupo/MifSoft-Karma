d35fffaa0440d15e5a619e82f6d75b3bdcaff171
Domantas Gruodis <domantas.gruod@gmail.com>
2021-10-22 01:09:18 +0300

    Adding some kind of validation

--- Care/Controllers/PostController.cs
+++ Care/Controllers/PostController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Care.Controllers;
 using Care.Models;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace Care.Controllers
 {
@@ -18,7 +19,7 @@ namespace Care.Controllers
             _context = context;
         }
         public IActionResult Index()
-        {
+        {   
             return View();
         }
         public IActionResult AddOrg()
@@ -26,25 +27,51 @@ namespace Care.Controllers
             return PartialView("/Views/Shared/_OrgBuilder.cshtml");
         }
         [HttpPost]
-        public async Task<ViewResult> AddOrg(NewPostModel newPostModel)
+        public async Task<IActionResult> AddOrg(NewPostModel newPostModel)
         {
+            ///It is possible to write more concise version of the code, but we need to use the optional argument.
+            ///We can delete this code and make it simpler after the assignment.
+            ///Also named params are redundant, since from variable names it is clear, what we try to pass.
+
             PostModel post = new PostModel();
 
-            post.OrgName = newPostModel.OrgName;
+            checkInput(OrgName: newPostModel.OrgName, newPostModel.OrgPhoto, post, newPostModel.OrgShortDescr, newPostModel.OrgLongDescr, newPostModel.OrgLink, newPostModel.OrgLogo);
 
-            post.OrgShortDescr = newPostModel.OrgShortDescr;
+            if(post == null)
+            {
+                return View("Index");
+            }
+            else
+            {
+                _context.Add(post);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index", "Post");
+            }      
+        }
 
-            post.OrgLongDescr = newPostModel.OrgLongDescr;
+        private void checkInput(string OrgName, string? OrgPhoto = "Images/DefaultOrgPhoto.png", params Object[] args)
+        {
+            PostModel tempPost = (PostModel)args[0];
+            for(int i = 1; i < args.Length; ++i)
+            {
+                if (args[i] == null)
+                {
+                    tempPost = null;
+                    return;
+                }
+            }
 
-            post.OrgLink = newPostModel.OrgLink;
+            tempPost.OrgName = OrgName;
 
-            post.OrgLogo = newPostModel.OrgLogo;
+            tempPost.OrgShortDescr = (string)args[1];
 
-            post.OrgPhoto = newPostModel.OrgPhoto;
+            tempPost.OrgLongDescr = (string)args[2];
 
-            _context.Add(post);
-            await _context.SaveChangesAsync();
-            return View("~/Views/Home/Index.cshtml");
+            tempPost.OrgLink = (string)args[3];
+
+            tempPost.OrgLogo = (string)args[4];
+
+            tempPost.OrgPhoto = OrgPhoto;
         }
     }
 }
