55cad29b9ffc4f4914a61d497cf980476edb9ce2
Nenartavičiūtė Laura <laura.nenartaviciute@mif.stud.vu.lt>
2021-10-21 20:56:12 +0300

    WIP

--- Care/Controllers/HomeController.cs
+++ Care/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using Care.Models;
+using Care.Helpers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -23,8 +25,24 @@ namespace Care.Controllers
 
         public async Task<IActionResult> Index()
         {
-            Posts<PostModel> postList = new Posts<PostModel>(db.Posts.ToArray());
-            return View(postList);
+ 
+            List<PostModel> postList = new List<PostModel>(db.Posts);
+
+            // 1, 0, or -1 is returned depending on whether one value is greater than, equal to, or less than the other. 
+             postList.Sort((PostModel x, PostModel y) => 
+             x.OrgName == null && y.OrgName == null
+                ? 0
+                : x.OrgName == null
+                    ? -1
+                    : y.OrgName == null
+                        ? 1
+                        : x.OrgName.CompareTo(y.OrgName));
+            //Array.Sort(postList, PostModel.SortByNameAscending());
+            
+
+            Posts <PostModel> postList2 = new Posts<PostModel>(postList.ToArray());
+
+            return View(postList2);
         }
 
         public IActionResult Privacy()
