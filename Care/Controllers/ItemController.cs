cfb63fa51b0abbe20e9e1e02927a85f684029efb
Edvinas <edvinas.krupo@gmail.com>
2021-11-05 15:07:15 +0200

    Add Email To Item Index

--- Care/Controllers/ItemController.cs
+++ Care/Controllers/ItemController.cs
@@ -9,6 +9,7 @@ using Care.Models;
 using Microsoft.AspNetCore.Hosting;
 using System.IO;
 using Microsoft.AspNetCore.Http;
+using System.Dynamic;
 
 namespace Care.Controllers
 {
@@ -26,7 +27,10 @@ namespace Care.Controllers
         // GET: Item
         public async Task<IActionResult> Index()
         {
-            return View(await _context.Items.ToListAsync());
+            var UsersAndItems = new UserAndItemModel();
+            UsersAndItems.Users = await _context.Users.ToListAsync();
+            UsersAndItems.Items = await _context.Items.ToListAsync();
+            return View(UsersAndItems);
         }
 
         // GET: Item/Details/5
---
+++ Care/Models/UserAndItemModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Care.Models
+{
+    public class UserAndItemModel
+    {
+        public IEnumerable<ItemModel> Items { get; set; }
+        public IEnumerable<UserModel> Users { get; set; }
+    }
+}
